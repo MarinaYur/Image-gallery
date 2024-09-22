7cbb8edbc21cc05f5ddf1dd6a0e7828dc66733ba
MarinaYur <marina.yurkev@gmail.com>
2024-09-22 19:42:12 +0300

    fix: fix paths in play-list.js

--- audio-player/src/js/play-list.js
+++ audio-player/src/js/play-list.js
@@ -4,24 +4,24 @@ export const playList = [
     singer: 'Cher',
     songTitle: 'Strong Enough',
     totalTime: '3:33',
-    songPath: '../src/assets/songs/CherStrongEnough.mp3',
-    coverPath: '../src/assets/cover/cher-Strong-Enaugh.jpeg'
+    songPath: './src/assets/songs/CherStrongEnough.mp3',
+    coverPath: './src/assets/cover/cher-Strong-Enaugh.jpeg'
   },
   {
     id: 1,
     singer: 'Justin Timberlake',
     songTitle: 'Like I Love You',
     totalTime: '4:47',
-    songPath: '../src/assets/songs/Justin Timberlake - Like I Love You.mp3',
-    coverPath: '../src/assets/cover/timberlake.jpeg'
+    songPath: './src/assets/songs/Justin Timberlake - Like I Love You.mp3',
+    coverPath: './src/assets/cover/timberlake.jpeg'
   },
   {
     id: 2,
     singer: 'Jenifer Lopes',
     songTitle: "Ain't Your Mama",
     totalTime: '3:38',
-    songPath: '../src/assets/songs/Jenifer lopes - Aint Your Mama.mp3',
-    coverPath: '../src/assets/cover/lopesAintYourMama.jpg'
+    songPath: './src/assets/songs/Jenifer lopes - Aint Your Mama.mp3',
+    coverPath: './src/assets/cover/lopesAintYourMama.jpg'
   }
 ]
 
