19fa2becef9f3621f86bfd99bdb8c4bda6363b39
MarinaYur <marina.yurkev@gmail.com>
2024-09-22 19:18:01 +0300

    feat: add currentTime count

--- audio-player/src/js/index.js
+++ audio-player/src/js/index.js
@@ -1,5 +1,4 @@
 import { playList } from "./play-list.js";
-// import { playAudio } from "./play-pause.js";
 
 let isPlay = false;
 let trackNumber = 0;
@@ -8,11 +7,13 @@ const playerSinger = document.querySelector(".player__singer");
 const playerSongTitle = document.querySelector(".player__song-title");
 const cover = document.querySelector(".player__cover");
 const totalTime = document.querySelector(".player__total-time");
+const playerCurrentTime = document.querySelector(".player__current-time");
+
 const nextAudioBtn = document.querySelector(".forward");
 const prevAudioBtn = document.querySelector(".backward");
 
 const playListLength = playList.length;
-const audio = new Audio('../src/assets/songs/CherStrongEnough.mp3');
+const audio = new Audio("../src/assets/songs/CherStrongEnough.mp3");
 
 export const playAudio = () => {
   if (!isPlay) {
@@ -26,36 +27,45 @@ export const playAudio = () => {
     isPlay = false;
     audio.pause();
   }
-}
+  setInterval(setCurrentTime, 1000);
+};
+
+export const setCurrentTime = () => {
+  const currentTime = audio.currentTime;
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = Math.floor(currentTime % 60);
+  const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  playerCurrentTime.innerHTML = formattedTime;
+};
 
 export const switchTrack = () => {
   if (audio.src) {
     audio.pause();
-}
+  }
   audio.src = playList[trackNumber].songPath;
   audio.currentTime = 0;
+
   fillSongData();
   isPlay ? audio.play() : audio.pause();
-}
+};
 
 export const fillSongData = () => {
   playerSinger.innerHTML = playList[trackNumber].singer;
   playerSongTitle.innerHTML = playList[trackNumber].songTitle;
   cover.style.background = `url(${playList[trackNumber].coverPath})`;
-  cover.style.backgroundSize = 'cover';
+  cover.style.backgroundSize = "cover";
 };
 
 export const switchNextAudio = () => {
-  trackNumber < playListLength - 1 ? trackNumber += 1 : trackNumber = 0;
+  trackNumber < playListLength - 1 ? (trackNumber += 1) : (trackNumber = 0);
   switchTrack();
 };
 
 const switchPrevAudio = () => {
-  trackNumber === 0 ? trackNumber = playListLength - 1 : trackNumber -= 1;
+  trackNumber === 0 ? (trackNumber = playListLength - 1) : (trackNumber -= 1);
   switchTrack();
 };
 
 playPauseBtn.addEventListener("click", playAudio);
-
 nextAudioBtn.addEventListener("click", switchNextAudio);
 prevAudioBtn.addEventListener("click", switchPrevAudio);
