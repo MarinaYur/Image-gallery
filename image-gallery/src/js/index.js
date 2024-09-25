6f908e25decfc5366bf38499e3d81a2c5688a472
MarinaYur <marina.yurkev@gmail.com>
2024-09-25 14:42:16 +0300

    feat: add function for searching of user request

--- image-gallery/src/js/index.js
+++ image-gallery/src/js/index.js
@@ -6,38 +6,43 @@ const gallery = document.querySelector('.gallery');
 
 let query = 'nature';
 let perPage = '20';
+let msgAboutNullAnswer = 'Sorry, nothing was found for your query. Try changing the query and re-enabling the search.';
 
+const createRequestWithNewURL = () => {
+  gallery.innerHTML = '';
+  query = inputValue.value;
+  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=${perPage}&orientation=landscape&client_id=xuDc8Ih3-wl4c13_mA0xvJuazv8R68rZ0gvEEDFk4K8`;
+  getData(url);
+};
 
-submit.addEventListener('click', () => {
-console.log(inputValue.value);
-});
-// let userRequestInput = inputValue.value;
-// if (inputValue.value) {
-  // console.log(inputValue);
-// }
+submit.addEventListener('click', createRequestWithNewURL);
 
 const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=${perPage}&orientation=landscape&client_id=xuDc8Ih3-wl4c13_mA0xvJuazv8R68rZ0gvEEDFk4K8`;
 
-async function getData() {
+async function getData(url) {
+  console.log(url);
   const res = await fetch(url);
   const data = await res.json();
   const answerArray = data.results;
-  console.log(data.results[0].urls.regular);
-  if (res.status === 200) {
-    if (answerArray.length === 0) {
-      showMsgAboutNullAnswer();
+  // console.log(data.results[0].urls.regular);
+  if ((res.status === 200 && answerArray.length === 0) || res.status === 400) {
+      showMsgAboutNullAnswer(msgAboutNullAnswer);
     } else {
       answerArray.forEach(img => {
         showAnswerImg(img.alt_description, img.urls.regular)
       });
     }
   }
-}
-getData();
+  // else {
 
-const showMsgAboutNullAnswer = () => {
+  // }
+// }
+getData(url);
+
+const showMsgAboutNullAnswer = (msgAboutNullAnswer) => {
   const p = document.createElement('p');
   p.classList.add('gallery__msg-null-answer');
+  p.innerHTML = msgAboutNullAnswer;
   gallery.append(p);
 };
 
