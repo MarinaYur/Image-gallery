c283be9291119e39294183ac87d6fc54cb54634b
MarinaYur <marina.yurkev@gmail.com>
2024-09-27 15:27:26 +0300

    feat: add function to clear search line

--- image-gallery/src/js/index.js
+++ image-gallery/src/js/index.js
@@ -1,42 +1,40 @@
 
 
 const submit = document.querySelector('.header__search-icon');
-let inputValue = document.querySelector('.header-input');
+let input = document.querySelector('.header-input');
 const gallery = document.querySelector('.gallery');
+const clear = document.querySelector('.header__clear');
 
 let query = 'nature';
-let perPage = '20';
+let perPage = '28';
 let msgAboutNullAnswer = 'Sorry, nothing was found for your query. Try changing the query and re-enabling the search.';
+let msgServerIsNotResponding = 'The server is not responding. Please try again';
 
 const createRequestWithNewURL = () => {
   gallery.innerHTML = '';
-  query = inputValue.value;
+  query = input.value;
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=${perPage}&orientation=landscape&client_id=xuDc8Ih3-wl4c13_mA0xvJuazv8R68rZ0gvEEDFk4K8`;
   getData(url);
 };
 
-submit.addEventListener('click', createRequestWithNewURL);
+
 
 const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=${perPage}&orientation=landscape&client_id=xuDc8Ih3-wl4c13_mA0xvJuazv8R68rZ0gvEEDFk4K8`;
 
 async function getData(url) {
-  console.log(url);
   const res = await fetch(url);
   const data = await res.json();
   const answerArray = data.results;
-  // console.log(data.results[0].urls.regular);
   if ((res.status === 200 && answerArray.length === 0) || res.status === 400) {
       showMsgAboutNullAnswer(msgAboutNullAnswer);
-    } else {
+    } else if (res.status === 200) {
       answerArray.forEach(img => {
         showAnswerImg(img.alt_description, img.urls.regular)
       });
+    } else {
+      showMsgAboutNullAnswer(msgServerIsNotResponding)
     }
   }
-  // else {
-
-  // }
-// }
 getData(url);
 
 const showMsgAboutNullAnswer = (msgAboutNullAnswer) => {
@@ -51,9 +49,24 @@ const showAnswerImg = (alt,url) => {
   const img = document.createElement('img');
   img.alt = alt;
   img.src = url;
-
   div.classList.add('gallery__picture');
-
   gallery.append(div);
   div.append(img);
 };
+
+submit.addEventListener('click', createRequestWithNewURL);
+
+document.addEventListener( 'keyup', event => {
+  if( event.code === 'Enter' && document.activeElement === input) createRequestWithNewURL();
+});
+
+document.addEventListener( 'input', event => {
+  if (input.value) {
+    clear.classList.remove('header__clear');
+  } else clear.classList.add('header__clear');
+});
+
+clear.addEventListener('click', () => {
+  input.value = '';
+  clear.classList.add('header__clear');
+})
